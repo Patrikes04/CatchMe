b85f2a236b2aa0bc486168afae0d14f88636e295
Patrikes04 <patrikes@wp.pl>
2019-01-03 22:49:07 +0100

    Adding Bat File + small cleaning fixing

--- src/Clear.java
+++ src/Clear.java
@@ -1,5 +1,5 @@
 public class Clear {
     public static void cleaning() {
-        for (int i = 0; i < 20; ++i) System.out.println();
+        for (int i = 0; i < 30; ++i) System.out.println();
     }
 }
